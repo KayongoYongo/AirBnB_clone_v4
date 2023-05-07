0aa8b1443e926069606d3988659f64c8b0ed917d
KayongoYongo <sknsammie10@gmail.com>
2023-05-07 16:00:48 +0000

    javascript

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  // Variable to store the checked amenities
+  var checkedAmenities = {};
+
+  // Listen for changes on input checkbox tags
+  $('.amenity-checkbox').change(function() {
+    var amenityId = $(this).data('id');
+    var amenityName = $(this).data('name');
+
+    // Check if the checkbox is checked
+    if ($(this).is(':checked')) {
+      // Store the Amenity ID in the variable
+      checkedAmenities[amenityId] = amenityName;
+    } else {
+      // Remove the Amenity ID from the variable
+      delete checkedAmenities[amenityId];
+    }
+
+    // Update the h4 tag inside the div Amenities with the list of checked Amenities
+    var amenitiesList = Object.values(checkedAmenities).join(', ');
+    $('.amenities h4').text(amenitiesList);
+  });
+});
